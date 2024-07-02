7ac6bf9f58c6f775f49d8bb9e0a6e51930eb4b60
Hubert <kiszkahub@gmail.com>
2024-07-02 13:02:53 +0200

    init

--- LAB1.py
+++ LAB1.py
@@ -6,5 +6,11 @@ data = pd.read_excel("files/lab 1/practice_lab_1.xlsx")
 nazwy_kolumn = list(data.columns)
 wartosci_kolumn = np.array(data.values)
 np.set_printoptions(suppress=True,precision=2)
-print(nazwy_kolumn)
-print(wartosci_kolumn)
+
+def zadanie1_2():
+    #1.2.1
+    res = wartosci_kolumn[::2,:] - wartosci_kolumn[1::2,:]
+    #1.2.2
+
+
+zadanie1_2()
