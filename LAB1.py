958ebe1b4d343b4ffce55763c9dd23a3ba4319a5
Hubert <kiszkahub@gmail.com>
2024-07-03 11:08:18 +0200

    init

--- LAB1.py
+++ LAB1.py
@@ -23,5 +23,21 @@ def zadanie1_2():
     mean_cols = wartosci.mean(axis=0)
     gt_mean = wartosci > mean_cols
     count_gt = gt_mean.sum(axis=0)
+    #1.2.7
+    max_val = wartosci.max()
+    cols_with_max = wartosci == max_val
+    cols_with_true = np.any(cols_with_max,axis=0)
+    cols_index = np.where(cols_with_true)[0]
+    #print([nazwy_kolumn[i] for i in cols_index])
+    #1.2.8
+    cols_masked = wartosci == 0
+    count_of_zero_in_cols = np.sum(cols_masked, axis=0)
+    max_index = np.argmax(count_of_zero_in_cols)
+    # print(nazwy_kolumn[max_index])
+    #1.2.9
+    cols = wartosci[::2,].sum(axis=0) > wartosci[1::2,].sum(axis=0)
+    idx = np.where(cols)[0]
+    print([nazwy_kolumn[i] for i in idx])
+
 
 zadanie1_2()
