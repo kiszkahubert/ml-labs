fe0ae850c39974273f22543cbae4edf5ea932859
Hubert <kiszkahub@gmail.com>
2024-07-03 10:46:30 +0200

    init

--- LAB1.py
+++ LAB1.py
@@ -4,13 +4,24 @@ import pandas as pd
 
 data = pd.read_excel("files/lab 1/practice_lab_1.xlsx")
 nazwy_kolumn = list(data.columns)
-wartosci_kolumn = np.array(data.values)
+wartosci = np.array(data.values)
 np.set_printoptions(suppress=True,precision=2)
 
 def zadanie1_2():
     #1.2.1
-    res = wartosci_kolumn[::2,:] - wartosci_kolumn[1::2,:]
+    res1 = wartosci[::2, :] - wartosci[1::2, :]
     #1.2.2
-
+    res2 = (wartosci - wartosci.mean())/wartosci.std()
+    #1.2.3
+    res3 = (wartosci - wartosci.mean(axis=0))/(wartosci.std(axis=0)+np.spacing(wartosci.std(axis=0)))
+    #1.2.4
+    res4 = (wartosci.mean(axis=0)/(wartosci.std(axis=0)+np.spacing(wartosci.std(axis=0))))
+    #1.2.5
+    max_index = np.argmax(res4)
+    max_value = res4[max_index]
+    #1.2.6
+    mean_cols = wartosci.mean(axis=0)
+    gt_mean = wartosci > mean_cols
+    count_gt = gt_mean.sum(axis=0)
 
 zadanie1_2()
