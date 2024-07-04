8a26a22615bd46288cbba73b40377418762ca2aa
Hubert <kiszkahub@gmail.com>
2024-07-04 17:40:16 +0200

    init

--- LAB3.py
+++ LAB3.py
@@ -6,12 +6,17 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
-def zadanie3_1():
+def zadanie3_2(data,column,value_to_be_1):
+    mask = data[column].values == value_to_be_1
+    data.loc[mask,column] = 1
+    data.loc[~mask,column] = 0
+    return data
+
+def zadanie3_3(data):
     pass
 
 if __name__ == "__main__":
     data = pd.read_excel('files/lab 3/practice_lab_3.xlsx')
-    columns = list(data.columns)
-    mask = data['Gender'].values == 'Female'
-    data.loc[mask,'Gender'] = 1
-    data.loc[~mask,'Gender'] = 0
+    print(data.columns)
+    # data_modified = zadanie3_2(data.copy(),"Gender","Female")
+    
