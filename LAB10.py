1f35b2eb183226645cf6e0ed2b1f5ce2868d69d5
Hubert <kiszkahub@gmail.com>
2024-07-18 17:10:26 +0200

    init

--- LAB10.py
+++ LAB10.py
@@ -5,7 +5,7 @@ import os
 from keras.utils.vis_utils import plot_model
 from keras.datasets import fashion_mnist, mnist
 from keras.models import Model, Sequential, model_from_json
-from keras.layers import Conv2D, MaxPool2D,GlobalAveragePooling2D,Dense,Input,Reshape,UpSampling2D,BatchNormalization,GaussianNoise
+from keras.layers import Conv2D, MaxPool2D,GlobalAveragePooling2D,Dense,Input,Reshape,MaxPooling2D,UpSampling2D,BatchNormalization,GaussianNoise, Dropout
 from keras.optimizers import Adam
 from keras import backend as K
 from matplotlib import pyplot as plt
@@ -117,6 +117,76 @@ def autoencoder_conv(X_train):
 
     autoencoder.save_weights(os.path.join(save_dir, "autoencoder_noise_weights.h5"))
 
+#zadanie 10.2
+# def own_autoencoder(X_train):
+#     act_func = 'relu'
+#     encoder_layers = [GaussianNoise(1),
+#                       Conv2D(32,(3,3),padding='same',activation=act_func),
+#                       MaxPool2D(2,2),
+#                       Dropout(0.25),
+#                       Conv2D(64,(3,3),padding='same',activation=act_func),
+#                       MaxPool2D(2,2),
+#                       BatchNormalization(),
+#                       Conv2D(128,(3,3),padding='same', activation=act_func)]
+    
+#     decoder_layers = [UpSampling2D((2,2)),
+#                       Conv2D(32,(3,3),padding='same',activation=act_func),
+#                       BatchNormalization(),
+#                       UpSampling2D((2,2)),
+#                       Conv2D(32,(3,3),padding='same',activation=act_func),
+#                       Dropout(0.25),
+#                       Conv2D(1,(3,3),padding='same',activation='sigmoid')]
+    
+#     learning_rate = 0.0001
+#     tensor = autoencoder_input = Input(X_train.shape[1:])
+#     for layer in encoder_layers+decoder_layers:
+#         tensor = layer(tensor)
+    
+#     autoencoder = Model(inputs=autoencoder_input,outputs=tensor)
+#     autoencoder.compile(optimizer=Adam(learning_rate),loss='binary_crossentropy')
+#     autoencoder.fit(x=X_train,y=X_train,epochs=50,batch_size=256)
+#     save_dir = "./saved_models"
+#     model_json = autoencoder.to_json()
+#     with open(os.path.join(save_dir, "own_autoencoder.json"), "w") as json_file:
+#         json_file.write(model_json)
+
+#     autoencoder.save_weights(os.path.join(save_dir, "own_autoencoder.h5"))
+
+#zadanie 10.3
+def own_autoencoder(X_train):
+    act_func = 'relu'
+    encoder_layers = [GaussianNoise(0.1),
+                      Conv2D(32,(3,3),padding='same',activation=act_func),
+                      MaxPooling2D(2,2),
+                      Dropout(0.25),
+                      Conv2D(64,(3,3),padding='same',activation=act_func),
+                      MaxPooling2D(2,2),
+                      BatchNormalization(),
+                      Conv2D(128,(3,3),padding='same', activation=act_func)]
+    
+    decoder_layers = [Conv2D(128,(3,3),padding='same',activation=act_func),
+                      UpSampling2D((2,2)),
+                      BatchNormalization(),
+                      Conv2D(64,(3,3),padding='same',activation=act_func),
+                      UpSampling2D((2,2)),
+                      Conv2D(32,(3,3),padding='same',activation=act_func),
+                      Conv2D(1,(3,3),padding='same',activation='sigmoid')]
+    
+    learning_rate = 0.0001
+    tensor = autoencoder_input = Input(X_train.shape[1:])
+    for layer in encoder_layers+decoder_layers:
+        tensor = layer(tensor)
+    
+    autoencoder = Model(inputs=autoencoder_input,outputs=tensor)
+    autoencoder.compile(optimizer=Adam(learning_rate),loss='mean_squared_error')
+    autoencoder.fit(x=X_train,y=X_train,epochs=50,batch_size=256)
+    save_dir = "./saved_models"
+    model_json = autoencoder.to_json()
+    with open(os.path.join(save_dir, "own_autoencoder.json"), "w") as json_file:
+        json_file.write(model_json)
+
+    autoencoder.save_weights(os.path.join(save_dir, "own_autoencoder.h5"))
+
 def show_pictures(arrs):
     arr_cnt = arrs.shape[0]
     fig, ax = plt.subplots(1,arr_cnt,figsize=(5*arr_cnt,arr_cnt))
@@ -136,7 +206,7 @@ def add_salt_and_pepper_noise(images, salt_prob=0.1, pepper_prob=0.1):
 
 if __name__ == '__main__':
     tf.config.experimental.set_memory_growth(tf.config.experimental.list_physical_devices('GPU')[0], True)
-    (X_train,y_train), (X_test,y_test) = mnist.load_data()
+    (X_train,y_train), (X_test,y_test) = fashion_mnist.load_data()
     X_train = np.expand_dims(X_train, axis=-1) # /255.0
     X_train_scaled = (X_train/255).copy()
     X_test = np.expand_dims(X_test, axis=-1) / 255.0
@@ -168,11 +238,23 @@ if __name__ == '__main__':
     #         ax[-j-1,i].axis('off')
     # plt.show()
 
-    autoencoder = load_model('./saved_models/autoencoder_noise.json','./saved_models/autoencoder_noise_weights.h5')
+    #zadanie 10.1
+    # autoencoder = load_model('./saved_models/autoencoder_noise.json','./saved_models/autoencoder_noise_weights.h5')
+    # test_photos = X_test[10:20,...].copy()
+    # mask = np.random.randn(*test_photos.shape)
+    # noisy_test_photos = add_salt_and_pepper_noise(test_photos)
+    # cleaned_images = autoencoder.predict(noisy_test_photos)*255
+    # show_pictures(test_photos)
+    # show_pictures(noisy_test_photos)
+    # show_pictures(cleaned_images)
+
+    #zadanie 10.2
+    # own_autoencoder(X_train_scaled)
+    own_autoencoder = load_model('./saved_models/own_autoencoder.json','./saved_models/own_autoencoder.h5')
     test_photos = X_test[10:20,...].copy()
     mask = np.random.randn(*test_photos.shape)
     noisy_test_photos = add_salt_and_pepper_noise(test_photos)
-    cleaned_images = autoencoder.predict(noisy_test_photos)*255
+    cleaned_images = own_autoencoder.predict(noisy_test_photos)*255
     show_pictures(test_photos)
     show_pictures(noisy_test_photos)
     show_pictures(cleaned_images)
